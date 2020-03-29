f2ca8e24b03155aa63356c01b3bab73616564e23
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-29 18:14:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,57 +1,58 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
-
   configure do
     set :views, "app/views"
     enable :sessions
     set :session_secret, "password_security"
   end
-
   get "/" do
     erb :index
   end
-
   get "/signup" do
     erb :signup
   end
-
   post "/signup" do
-    #your code here
-
-  end
-
+   if params[:username] == ""  || params[:password] == ""
+		redirect '/failure'
+	else
+	  User.create(username: params[:username], password: params[:password])
+		redirect "/login"
+	 end
+end
   get '/account' do
-    @user = User.find(session[:user_id])
+    @name_of_user = User.find(session[:user_id])
     erb :account
   end
-
-
   get "/login" do
     erb :login
   end
-
   post "/login" do
-    ##your code here
-  end
-
+	 user = User.find_by(:username => params[:username])
+	 if user && user.authenticate(params[:password])
+		session[:user_id] = user.id
+		redirect '/account'
+	 else
+		redirect '/failure'
+	 end
+	end
+	get "/success" do
+		erb :account
+	 end
   get "/failure" do
     erb :failure
   end
-
   get "/logout" do
     session.clear
     redirect "/"
   end
-
   helpers do
     def logged_in?
       !!session[:user_id]
     end
-
     def current_user
       User.find(session[:user_id])
     end
   end
-
 end
